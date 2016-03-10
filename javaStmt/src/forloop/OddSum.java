7a85779d7e1b26b58d1cc0ba238588cf0e3bc619
자기이름 <@naver.com>
2016-03-10 11:12:21 +0900

    java statment 프로젝트 시작 !!

---
+++ javaStmt/src/forloop/OddSum.java
@@ -0,0 +1,26 @@
+package forloop;
+/**
+ *@file : OddSum.java 
+ *@author pakjkwan@gmail.com
+ *@date 2016. 3. 10.
+ *@story 1부터 10까지 홀수의 합
+ */
+public class OddSum {
+	public static void main(String[] args) {
+		int sum=0,i =0;
+		System.out.println("1번째 방법");
+		for (i = 1; i <= 10; i+=2) {
+			 sum +=i;
+		}
+		System.out.println("1부터 10까지 홀수의 합 " +sum);
+		System.out.println("2번째 방법");
+		for (i = 0; i < 10; i++) {
+			
+			if (i % 2 == 1) {
+				sum += i;
+			}
+			
+		}
+		System.out.println("1부터 10까지 홀수의 합 " +sum);
+	}
+}
